5a6debbeeb3f558f5bd06d3cce7b9584a3e135e2
Anna <anakseon@rambler.ru>
2023-07-30 12:10:57 +0300

    Домашнее задание к 6 семинару

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import robot.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals =new Animal[3];
+        Animal[] animals =new Animal[3];//рефакторим класс анимал
         animals[1] = new Dog("Dog",5,3,"Petro","Hi, i`m dog");
+        //рефакторим класс дог
         animals[2] = new Dog("Dog 2",3,1,"Vaska","i`m dog 2");
 
         for (Animal animal:animals) {
